529c95b346c472143078e1741e62a04123ffa0b9
Ivan Rodrigo Cruz Aguilar <167045012+Strydeerx@users.noreply.github.com>
2024-07-01 19:44:06 -0600

    Update datosper.component.ts

--- src/app/home/datosper/datosper.component.ts
+++ src/app/home/datosper/datosper.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup,FormBuilder,FormControl } from '@angular/forms';
+import { ApidatosService } from './apidatos.service';
+import { intdatos } from './datosper.interface';
 
 @Component({
   selector: 'app-datosper',
@@ -7,13 +9,99 @@ import { FormGroup,FormBuilder,FormControl } from '@angular/forms';
   styleUrls: ['./datosper.component.scss'],
 })
 export class DatosperComponent   {
-loginForm: FormGroup;
-  constructor(private fb: FormBuilder) { 
-    
-    this.loginForm = this.fb.group({
+
+  loginForm = new FormGroup({
+    idpersona: new FormControl(''),
+    nombre: new FormControl(''),
+    apellidop: new FormControl(''),
+    apellidom: new FormControl(''),
+    correo: new FormControl(''),
+    telefono: new FormControl(''),
+    idgenero: new FormControl(''),
+    idestado: new FormControl(''),
+    idpais: new FormControl(''),
   });
 
-  
-}
+  constructor(private api: ApidatosService) { }
+    
+    recuperarFormulario():intdatos{
+      const formValues = this.loginForm.value;
+      const user: intdatos = {
+        idpersona: formValues.idpersona!,
+        nombre: formValues.nombre!,
+        apellidop: formValues.apellidop!,
+        apellidom: formValues.apellidom!,
+        correo: formValues.correo!,
+        telefono: formValues.telefono!,
+        idgenero: formValues.idgenero!,
+        idestado: formValues.idestado!,
+        idpais: formValues.idpais!,
+      };
+      return user;
+    }
 
+    obtener(){
+      this.api.obtener().subscribe(data => {
+        console.log(data);
+      });
+    }
+    obtenerid(){
+      const id = this.loginForm.value.idpersona;
+      if (id){
+        console.log(`Consultando datos por id: ${id}`);
+        this.api.obtenerid(id).subscribe(data => {
+          if (data && data.body){
+            const dato = data.body;
+            this.loginForm.patchValue({
+              nombre: dato.nombre || ''
+            });
+            console.log('datos consultados:', dato);
+          } else {
+            console.log('No se encontraron datos por id', id);
+          }
+
+        }, error => {
+          console.error('Error al consultar por ID:', error);
+        });
+      } else {
+        console.warn('ID vacío, por favor ingresa un ID válido.');
+      }
+    }
+    agregar(){
+      const user = this.recuperarFormulario();
+      this.api.agregar(user).subscribe(data => {
+        console.log('datos agregado:', data);
+      }, error => {
+        console.error('Error al agregar usuario:', error);
+      });
+    }
+    editar(){
+      const id = this.loginForm.value.idpersona;
+      const user = this.recuperarFormulario();
+      if (id){
+      this.api.editar(id,user).subscribe(data => {
+        console.log('datos actualizados:', data);
+        this.loginForm.patchValue({ nombre:''});
+        this.obtener();
+      }, error => {
+        console.error('Error al editar datos:', error);
+      });
+    } else {
+      console.warn('ID vacío, por favor ingresa un ID válido.');
+    }
+  }
+  eliminar(){
+    const id = this.loginForm.value.idpersona;
+    if (id){
+      this.api.eliminar(id).subscribe(data => {
+        console.log('datos eliminados:', data);
+        this.loginForm.patchValue({ nombre:''});
+        this.obtener();
+      }, error => {
+        console.error('Error al eliminar datos:', error);
+      });
+    } else {
+      console.warn('ID vacío, por favor ingresa un ID válido.');
+    }
+  }
 }
